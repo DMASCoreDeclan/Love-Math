db37f8e83083299db21f67b4e4c26bc190f8ee15
DMASCoreDeclan <54984695+DMASCoreDeclan@users.noreply.github.com>
2023-08-14 11:47:52 +0000

    Add division functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,7 @@ function runGame(gameType) {
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
     } else if (gameType === "division") {
-        displayDivideQuestion(num1, num2);
+        displayDivideQuestion(num1, num2);      
     } else {
         alert(`Unknown game type: ${gameType}`)
         throw `Unknown game type: ${gameType}.  Aborting!`
@@ -136,13 +136,9 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivideQuestion(operand1, operand2) {
-    if (operand1 > operand2) {
-        document.getElementById('operand1').textContent = operand1;
-        document.getElementById('operator').textContent = "/";
-        document.getElementById('operand2').textContent = operand2;
-        } else {
-        document.getElementById('operand2').textContent = operand1;
-        document.getElementById('operator').textContent = "/";
-        document.getElementById('operand1').textContent = operand2;
-        }
+    operand1 = operand1 * operand2;
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operator').textContent = "/";
+    document.getElementById('operand2').textContent = operand2; 
 }
